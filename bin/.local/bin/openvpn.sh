4a7cf91a933632a4e9ed16403b5abe02eaec4f15
Sebastian Bolanos <sebasheston@gmail.com>
2018-05-11 12:17:07 -0700

    small change

--- bin/.local/bin/openvpn.sh
+++ bin/.local/bin/openvpn.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
-FULL_PATH=~/.local/bin/vpn_configs
+FULL_PATH=$HOME/.local/bin/vpn_configs
 config_file=$(ls $FULL_PATH | shuf -n1)
 path=$FULL_PATH/$config_file
 echo $path
 screen -L -S night sudo openvpn --config $path --auth-user-pass ~/.local/bin/login.auth
-#screen -L -S sssssh sudo openvpn --config $path --auth-user-pass ~/.local/bin/login.auth 2> ~/.local/bin/EXIT_CODE
+#sudo openvpn --config $path --auth-user-pass ~/.local/bin/login.auth
 
 echo $path
 #sudo openvpn --config $path --auth-user-pass ~/.local/bin/login.auth 2> ~/.local/bin/EXIT_CODE
