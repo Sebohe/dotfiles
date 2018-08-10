91a93ac0c48a2856751f4e4bf5358c36015542aa
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-10 13:14:29 -0700

    improved my pomodoro timer

--- bin/.local/bin/ocean.sh
+++ bin/.local/bin/ocean.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 amixer set Master 50%
+
 while :
 do
+  sed -i '18,28s|^#||g' ~/.hosts
   mpg123 -k 2000 $HOME/audios/ocean.mp3
   sleep 0.2
   mpg123 -k 2000 $HOME/audios/ocean.mp3
   sleep 0.2
+  sed -i '18,28s|^|#|g' ~/.hosts
   mpg123 $HOME/audios/deorro.mp3
   sleep 0.2
 done
