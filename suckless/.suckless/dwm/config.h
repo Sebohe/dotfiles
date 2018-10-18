d3ca5776dcdfe8a84b2ab472c9325231af69765f
Sebastian Bolanos <sebasheston@gmail.com>
2018-10-17 17:20:51 -0700

    Remove firefox from status bar location

--- suckless/.suckless/dwm/config.h
+++ suckless/.suckless/dwm/config.h
@@ -27,15 +27,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,      1 << 8,        0,           -1 },
-    { "Riot",     NULL,       NULL,       4,            0,             -1},
-    { "Telegram", NULL,       NULL,       4,            0,             -1},
-    { "Google",   NULL,       NULL,       65,         0,             -1},
-    { "Brave",   NULL,       NULL,       64,         0,             -1},
-    { "Chromium", NULL,       NULL,       128,         0,             -1},
-    { "Tor",      NULL,       NULL,       128,         0,             -1},
-    { "Firefox Developer Edition",      NULL,       NULL,       128,         0,             -1},
+  { "Riot",     NULL,       NULL,       4,            0,             -1},
+  { "Telegram", NULL,       NULL,       4,            0,             -1},
+  { "Google",   NULL,       NULL,       65,           0,             -1},
+  { "Brave",    NULL,       NULL,       64,           0,             -1},
+  { "Chromium", NULL,       NULL,       128,          0,             -1},
+  { "Tor",      NULL,       NULL,       128,          0,             -1},
 };
 
 /* layout(s) */
