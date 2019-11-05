8a0c4aa1bb213347f022effb97cc41be3a36c84b
Sebastian Bolanos <sebasheston@gmail.com>
2019-11-05 08:57:08 -0600

    Add nixos config

---
+++ nixos/configuration.nix
@@ -0,0 +1,84 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  boot.loader = {
+   # Use the systemd-boot EFI boot loader.
+   systemd-boot.enable = true;
+   systemd-boot.configurationLimit = 3;
+   systemd-boot.consoleMode = "keep";
+   timeout = 0;
+   efi.canTouchEfiVariables = true;
+   # grub.device = "/dev/sda";
+  };
+
+  networking = {
+   hostName = "mini"; # Define your hostname.
+   wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+   interfaces.enp0s25.useDHCP = true;
+   interfaces.wlp3s0.useDHCP = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Managua";
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    firefox
+    git
+    tmux
+    dwm
+    dmenu
+    binutils
+    gcc
+    gnumake
+    openssl
+    pkgconfig
+  ];
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    # caps locks boot
+    xkbOptions = "ctrl:nocaps";
+    autorun = true;
+    desktopManager.plasma5.enable = true;
+    # Enable touchpad support.
+    libinput.enable = true;
+  };
+
+  users = {
+    defaultUserShell = pkgs.zsh;
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.sebas = {
+      # The set of packages that should be made availabe to the user. This is in
+      # contrast to environment.systemPackages, which adds packages to all users. 
+      packages = with pkgs; [
+        cargo
+        rustup
+      ];
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    };
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+}
