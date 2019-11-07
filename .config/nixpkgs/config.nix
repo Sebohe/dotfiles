b907fd4846a71ec5f9f260a3d26d761e180211ae
Sebastian Bolanos <sebasheston@gmail.com>
2019-11-07 15:29:39 -0600

    Add seperate myPackages override

---
+++ .config/nixpkgs/config.nix
@@ -0,0 +1,39 @@
+with import <nixpkgs> {};
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "myPackages";
+      paths = [
+        chromium
+        vscode
+        signal-desktop
+        rustracer
+        vlc
+        alacritty
+        scrot
+        feh
+        tdesktop
+        dwm-status
+        dunst
+        pass
+        gitAndTools.gitFull
+        gitAndTools.git-annex
+      	gitAndTools.gitRemoteGcrypt
+        vscode
+        rustup
+        neovim
+        lsd
+        firefox
+        tmux
+        libreoffice-fresh
+        (dwm.override {
+          patches =
+            [ /home/sebas/.dotfiles/suck/dwm/dwm-config.patch ];
+        })
+        dmenu
+      ];
+      pathsToLink = [ "/share" "/bin" ];
+    };
+  };
+}
