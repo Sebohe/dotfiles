fd89eca089a2e1b69f33ebe199457d02bccf9964
Sebastian Bolanos <sebasheston@protonmail.com>
2020-04-13 11:27:23 +0100

    Change brightness

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z $2 ]]; then
-    xrandr --output eDP-1 --brightness $1
+    xrandr --output eDP --brightness $1
 else
-    xrandr --output eDP-1 --brightness $1 --output HDMI-1 --brightness $2
+    xrandr --output eDP --brightness $1 --output HDMI-1 --brightness $2
 fi
