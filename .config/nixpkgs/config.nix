2af001486168b18d0991a57710e74a9741b65352
Sebastian Bolanos <sebasheston@protonmail.com>
2021-07-13 11:43:02 +0200

    Restrucutre mynixpkgs to use myAll

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,121 +1,149 @@
-with import <nixpkgs-unstable> {};
-{
+{ pkgs ? import <nixpkgs-unstable> {} }:
+let
+  sc2 = {
+    name = "sc2";
+    paths = with pkgs; [
+      wine-staging
+      winetricks
+      libpulseaudio
+      libjpeg_turbo
+      libpng
+    ];
+  };
+  browsers = {
+    name = "myBrowsers";
+    paths = with pkgs; [
+      #tor-browser-bundle-bin
+      firefox-devedition-bin
+      ungoogled-chromium
+    ];
+  };
+  art = {
+    name = "myArt";
+    paths = with pkgs; [
+      jack2Full
+      a2jmidid
+      qjackctl
+      python38Packages.foxdot
+      supercollider
+      gimp
+      audacity
+    ];
+  };
+  ui = {
+    name = "myUI";
+    paths = with pkgs; [
+      (dwm.override {
+        patches =
+          [
+            /home/sebas/.dotfiles/dwm/dwm-config.patch
+          ];
+      })
+      (import /home/sebas/repos/programs/romodoro {inherit pkgs;})
+      #(import /home/sebas/repos/programs/dwm-status {})
+      dmenu
+      feh
+      dunst
+      i3lock-color
+      libnotify
+      scrot
+      alacritty
+      pass
+      pavucontrol
+      xclip
+      lsd
+      xbindkeys
+      transmission-gtk
+      yubioath-desktop
+      blueman
+      lm_sensors
+      lshw
+      pinentry
+      qtpass
+      starship
+      #obinskit
+    ];
+  };
+  programming = {
+    name = "myProgramming";
+    paths = with pkgs; [
+      ripgrep
+      rustup
+      go_1_16
+      morph
+      terraform_0_15
+      dive
+      nixops
+      fd
+      fzf
+      lazydocker
+      gitAndTools.gitFull
+      gitAndTools.diff-so-fancy # needed for diff-highlight?
+      #R
+      rnix-lsp
+      neovim
+      niv
+    ];
+  };
+  messaging = {
+    name = "myMessaging";
+    paths = with pkgs; [
+      #zoom-us
+      discord
+      signal-desktop
+      #tdesktop
+      element-desktop
+    ];
+  };
+  tools = {
+    name = "myTools";
+    paths = with pkgs; [
+      age
+      cheat
+      gitAndTools.git-annex
+      gitAndTools.gitRemoteGcrypt
+      #pythonPackages38.youtube-dl
+      jq
+      unzip
+      ncdu
+      xorg.xev
+      ipcalc
+      whois
+      tcpdump
+      libreoffice-fresh
+      vlc
+      nomacs
+      breeze-icons
+      speedcrunch #caculator
+      dolphin # file manager
+      calibre # ebooks
+      gptfdisk
+      qrencode
+      pciutils
+      arandr
+      sshuttle
+      usbutils
+      exiftool
+    ];
+  };
+in {
   allowUnfree = true;
   permittedInsecurePackages = [
-    "p7zip-16.02"
+    #"p7zip-16.02"
   ];
-  packageOverrides = pkgs: with pkgs; {
-    mySc2 = pkgs.buildEnv {
-      name = "sc2";
-      paths = [
-        wine-staging
-        winetricks
-        libpulseaudio
-        libjpeg_turbo
-        libpng
-      ];
-    };
-    myArt = pkgs.buildEnv {
-      name = "myArt";
-      paths = [
-        jack2Full
-        a2jmidid
-        qjackctl
-        python38Packages.foxdot
-        supercollider
-        gimp
-        audacity
-      ];
-    };
-    myBrowsers = pkgs.buildEnv {
-      name = "myBrowsers";
-      paths = [
-        #tor-browser-bundle-bin
-        firefox-devedition-bin
-        chromium
-        #brave
-        #spotify
-      ];
-    };
-    myUI = pkgs.buildEnv {
-      name = "myUI";
-      paths = [
-        # UI
-        (dwm.override {
-          patches =
-            [
-              /home/sebas/.dotfiles/dwm/dwm-config.patch
-            ];
-        })
-        dwm-status # this is the rust version
-        dmenu
-        feh
-        dunst
-        tmux
-        i3lock-color
-        libnotify
-        scrot
-        alacritty
-        pass
-        pavucontrol
-        xclip
-        lsd
-        xbindkeys
-        transmission-gtk
-      ];
-    };
-    myProgramming = pkgs.buildEnv {
-      name = "myProgramming";
-      paths = [
-        ripgrep
-        rustup
-        go_1_15
-        ansible
-        docker-compose
-        lazydocker
-        #dive # analyze docker image layers
-        lazygit
-        gitAndTools.gitFull
-        gitAndTools.diff-so-fancy # needed for diff-highlight?
-        #R
-        pipenv
-      ];
-    };
-    myMessaging = pkgs.buildEnv {
-      name = "myMessaging";
-      paths = [
-        #zoom-us
-        discord
-        signal-desktop
-        #tdesktop
-        element-desktop
-      ];
-    };
-    myTools = pkgs.buildEnv {
-      name = "myTools";
-      paths = [
-        gitAndTools.git-annex
-        gitAndTools.gitRemoteGcrypt
-        #pythonPackages38.youtube-dl
-        jq
-        unzip
-        ncdu
-        xorg.xev
-        ipcalc
-        whois
-        tcpdump
-        libreoffice-fresh
-        vlc
-        taskwarrior
-        nomacs
-        breeze-icons
-        speedcrunch #caculator
-        dolphin # file manager
-        calibre # ebooks
-        gptfdisk
-        qrencode
-        pciutils
-      ];
+
+  packageOverrides = pkgs: rec {
+    #myPenrose =  (import /home/sebas/repos/programs/my-penrose);
+    mySc2 = pkgs.buildEnv sc2;
+    myBrowsers = pkgs.buildEnv browsers;
+    myArt = pkgs.buildEnv art;
+    myUi = pkgs.buildEnv ui;
+    myProgramming = pkgs.buildEnv programming;
+    myMessaging = pkgs.buildEnv messaging;
+    myTools = pkgs.buildEnv tools;
+    myAll = pkgs.buildEnv {
+      name = "myAll";
+      paths = tools.paths ++ programming.paths ++ messaging.paths ++ ui.paths ++ browsers.paths;
     };
   };
 }
